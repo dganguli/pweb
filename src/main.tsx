67bdcceca84016daba91aa81ed7ae6f9582fceb9
Deep Ganguli <deep.ganguli@gmail.com>
2025-02-13 13:32:37 -0800

    react-snap to get html

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { hydrateRoot, createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const rootElement = document.getElementById('root')
+
+// If the root element has children, hydrate it (for pre-rendered content)
+if (rootElement?.hasChildNodes()) {
+  hydrateRoot(rootElement, 
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+} else {
+  // Otherwise create a new root (for client-side rendering)
+  createRoot(rootElement!).render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+}
